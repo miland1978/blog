733526097ae09b0ce93ddadc58af3a958b6d7a44
AndrewM <7352214+miland1978@users.noreply.github.com>
2017-12-04 14:51:02 +0100

    Generation of personal data

---
+++ datageneration/generate.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""This script generates personal info for mocking purpose"""
+
+import uuid
+import argparse
+from mimesis import Personal
+
+def parse_args():
+    """Configure parser and parse command line arguments"""
+    parser = argparse.ArgumentParser(description='Generate mock data.')
+    parser.add_argument('file', type=str, help='file to generate')
+    parser.add_argument('count', type=int, help='number of items to generate')
+    parser.add_argument('locale', metavar='locale', type=str, choices=['en', 'de', 'ru', 'ja'],
+                        help='locale for names')
+    return parser.parse_args()
+
+def generate(filename, count, locale):
+    """Generate and write data to the file"""
+    person = Personal(locale)
+    with open(filename, 'wt') as file:
+        for i in range(count):
+            file.write('{0}|{1}|{2}|{3}|{4}|{5}\n'.format(i, uuid.uuid4().hex, person.name(),
+                                                          person.surname(), person.email(),
+                                                          person.age()))
+
+if __name__ == '__main__':
+    vals = parse_args()
+    generate(vals.file, vals.count, vals.locale)
