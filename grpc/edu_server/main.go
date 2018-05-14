0ac6d6783efb11d50a8fb7843ad8bb9d45ffa145
miland1978 <7352214+miland1978@users.noreply.github.com>
2018-05-14 15:13:30 +0200

    proto added

---
+++ grpc/edu_server/main.go
@@ -0,0 +1,42 @@
+// Copyright DI Miliy Andrew 2018 All Rights Reserved.
+
+//go:generate protoc -I ../edu --go_out=plugins=grpc:../edu ../edu/catalog.proto
+
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/miland1978/blog/grpc/edu"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port = ":50051"
+)
+
+type server struct{}
+
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
+}
+
+func (s *server) SayHelloAgain(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	return &pb.HelloReply{Message: "Hello again " + in.Name}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
